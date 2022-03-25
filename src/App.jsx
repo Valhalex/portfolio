ede07f7421a218f3e6fbe729fb87a8d75caae98a
Isaac <iwallace96@outlook.com>
2022-03-25 01:51:51 -0400

    logo fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
+// import Menu from "./components/works/Works";
+
 import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
@@ -9,17 +11,16 @@ import {useState} from 'react'
 
 
 function App() {
-
   const [menuOpen,setMenuOpen] = useState(false)
   return (
-
     <div className="app">
      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+     {/* <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/> */}
      <div className="sections">
-       <Intro />
-       <Portfolio />
-       <Works />
-       <Testimonials />
+       <Intro/>
+       <Portfolio/>
+       <Works/>
+       <Testimonials/>
        <Contact/>
      </div>
     </div>
