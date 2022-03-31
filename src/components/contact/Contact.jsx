9ba8400bd41831f575fb510c62d65ddb39a3ef8d
Isaac <iwallace96@outlook.com>
2022-03-31 16:31:02 -0400

    update package

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,12 +2,16 @@ import { useState } from "react";
 import "./contact.scss";
 
 export default function Contact() {
-  const [message, setMessage] = useState(false);
-
-  const handleSubmit = (e) => {
+  const [message, setMessage] = useState("Submit");
+  // send form to my email
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setMessage(true);
+    setMessage("Sending...");
+    
+    
   };
+
+
   return (
     <div className="contact" id="contact">
       <div className="left">
@@ -15,11 +19,14 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact.</h2>
+
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
+          <input type="text" placeholder= "Full Name" required />
+          <input type="email" placeholder="Email" required />
+          <input type="text" placeholder="Subject Line" required/>
+          <textarea placeholder="Message" required></textarea>
           <button type="submit">Send</button>
-          {message && <span>Thanks, I'll reply ASAP :)</span>}
+          {/* {message && <span>Thanks, I'll reply ASAP :)</span>} */}
         </form>
       </div>
     </div>
