a7e753963814104e2ba64bb72128f7d21e134cad
Isaac <iwallace96@outlook.com>
2022-04-03 02:58:24 -0400

    website redesign

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
-
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import "./app.scss"
 import {useState} from 'react'
 import Menu from './components/menu/Menu'
 
+import { ParallaxProvider } from 'react-scroll-parallax'
+
 function App() {
   const [menuOpen,setMenuOpen] = useState(false)
   return (
+    <ParallaxProvider>
     <div className="app">
      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
@@ -21,6 +23,7 @@ function App() {
        <Contact/>
      </div>
     </div>
+    </ParallaxProvider>
   );
 }
 
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,49 +1,70 @@
 import "./intro.scss";
 import { init } from "ityped";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, } from "react";
+import { ParallaxBanner } from "react-scroll-parallax";
 
 export default function Intro() {
   const textRef = useRef();
+
+
+
+
+
+
   // typing/blinking effect
   useEffect(() => {
     init(textRef.current, {
       showCursor: true,
       backDelay: 1500,
-      backSpeed:60,
-      strings: ["Frontend Developer", "Fullstack Developer", "Artist"],
+      backSpeed: 60,
+      strings: ["Frontend Developer", "Fullstack Developer", "Problem Solver"],
     });
   }, []);
 
+
+
   return (
     
-    <div className="intro" id="intro">
-      <div className="introBg"></div>
-      <div className="left">
-        <div className="imgContainer">
-          <img src="assets/me.png" alt="" />
-        </div>
-        {/* when using clicks they should see a short summary of 
-        who i am and my social sites */}
-        <div className="bio">
+    <ParallaxBanner className="intro"
+      layers={[
+        {
+          className:'bottom',
+          speed: -50,
+          scaleZ: 1.5
+        },
+        {
+          className:'gradient'
+        },
+        
+        {
+          className:'midground',
+          speed: -40
+        },
+        
+        {
+          className:'foreground',
+          speed: -10,
+        },
+      ]}
 
+    >
+      <div className="frontTextContainer">
+          <div className="wrapper">
+            <h2>Hi There, I'm</h2>
+            <h1>Isaac Wallace</h1>
+            <h3>
+              I am a <span ref={textRef}></span>
+            </h3>
+          </div>
+          <a href="#portfolio">
+            <img src="assets/down.png" alt="" />
+          </a>
         </div>
-      </div>
-      <div className="right">
-        <div className="wrapper">
-          <h2>Hi There, I'm</h2>
-          <h1>Isaac Wallace</h1>
-          <h3>
-            I am a <span ref={textRef}></span>
-          </h3>
-        </div>
-        <a href="#portfolio">
-          <img src="assets/down.png" alt="" />
-        </a>
-      </div>
+      </ParallaxBanner>
 
-      
-      
-      
-    </div>
+
+    
+
   );
 }
+
