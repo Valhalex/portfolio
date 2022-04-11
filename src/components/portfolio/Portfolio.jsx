243f9c39453f0b8ebee6be6679982a78a45fe67b
Isaac <iwallace96@outlook.com>
2022-04-11 02:18:46 -0400

    responsiveness imented

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -50,31 +50,7 @@ export default function Portfolio() {
         </div>
 
       ))}
-      {webDevPorfolio.map((d) => (
-        
-        <div className="container">
-          
-          
-          <div className="top">
-            <img src={d.img} alt="" />
-          </div>
-          <div className="mid">
-            <div className="text">
-              <p>{d.description}</p>
-            </div>
-
-          </div>
-          <div className="bottom">
-            <div className="links">
-              <a href={d.github}><BsGithub /></a>
-              <a href={d.live} title="hello"><CgWebsite /></a>
-            </div>
-          </div>
-
-        </div>
-
-      ))}
-      
+     
 
     </div>
   );
